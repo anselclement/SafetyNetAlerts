dadd2efa43a5ffbfca0bf04761d414777873e4d5
Clement Ansel <clem-nogare@hotmail.fr>
2021-03-08 14:55:31 +0100

    configuration des niveaux de logs

--- src/main/java/com/safetynet/safetynetalerts/service/PersonService.java
+++ src/main/java/com/safetynet/safetynetalerts/service/PersonService.java
@@ -2,6 +2,8 @@ package com.safetynet.safetynetalerts.service;
 
 import com.safetynet.safetynetalerts.model.Person;
 import com.safetynet.safetynetalerts.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,8 @@ import java.util.Optional;
 @Service
 public class PersonService {
 
+    private static Logger logger = LoggerFactory.getLogger(PersonService.class);
+
     @Autowired
     PersonRepository personRepository;
 
@@ -19,6 +23,7 @@ public class PersonService {
     }
 
     public Iterable<Person> getPersons() {
+        logger.info("Récupération de la liste entière de personnes");
         return personRepository.findAll();
     }
 

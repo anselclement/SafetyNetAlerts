14f5d9f2afef18c27e079523bfeb8f65dd37ad25
Clement Ansel <clem-nogare@hotmail.fr>
2021-03-08 14:22:05 +0100

    mise en place du PersonController

---
+++ src/main/java/com/safetynet/safetynetalerts/controller/PersonController.java
@@ -0,0 +1,19 @@
+package com.safetynet.safetynetalerts.controller;
+
+import com.safetynet.safetynetalerts.model.Person;
+import com.safetynet.safetynetalerts.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping("/persons")
+    public Iterable<Person> getPersons() {
+        return personService.getPersons();
+    }
+}

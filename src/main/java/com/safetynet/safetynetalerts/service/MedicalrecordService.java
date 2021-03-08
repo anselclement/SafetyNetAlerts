29eaba56149f0ceefc94b98d46c933c1168f35e6
Clement Ansel <clem-nogare@hotmail.fr>
2021-03-08 15:35:42 +0100

    mise en place du service MedicalrecordService

---
+++ src/main/java/com/safetynet/safetynetalerts/service/MedicalrecordService.java
@@ -0,0 +1,42 @@
+package com.safetynet.safetynetalerts.service;
+
+
+import com.safetynet.safetynetalerts.model.Medicalrecord;
+import com.safetynet.safetynetalerts.repository.MedicalrecordRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MedicalrecordService {
+
+    private static Logger logger = LoggerFactory.getLogger(MedicalrecordRepository.class);
+
+    @Autowired
+    MedicalrecordRepository medicalrecordRepository;
+
+    public Optional<Medicalrecord> getMedicalrecord(final Long id){
+        return medicalrecordRepository.findById(id);
+    }
+
+    public Iterable<Medicalrecord> getMedicalrecords() {
+        logger.info("Récupération de la liste entière des dossiers médicaux");
+        return medicalrecordRepository.findAll();
+    }
+
+    public void deleteMedicalrecord(final Long id){
+        medicalrecordRepository.deleteById(id);
+    }
+
+    public Medicalrecord saveMedicalrecord(Medicalrecord medicalrecord){
+        return  medicalrecordRepository.save(medicalrecord);
+    }
+
+    public Iterable<Medicalrecord> save(List<Medicalrecord> medicalrecords){
+        return medicalrecordRepository.saveAll(medicalrecords);
+    }
+}

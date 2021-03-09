1d1a9aee5c5f0cb826e75dd0f8ed696a0d7747a4
Clement Ansel <clem-nogare@hotmail.fr>
2021-03-09 11:01:45 +0100

    changement au niveau de l'auto increment

--- src/main/java/com/safetynet/safetynetalerts/model/Medicalrecord.java
+++ src/main/java/com/safetynet/safetynetalerts/model/Medicalrecord.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Medicalrecord {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "first_name")

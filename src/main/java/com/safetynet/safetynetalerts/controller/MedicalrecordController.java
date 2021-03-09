2e415c3af10438b52e5da263c646595538ceac1f
Clement Ansel <clem-nogare@hotmail.fr>
2021-03-09 16:36:09 +0100

    mise en place des vues persons, firestations, medicalrecords

--- src/main/java/com/safetynet/safetynetalerts/controller/MedicalrecordController.java
+++ src/main/java/com/safetynet/safetynetalerts/controller/MedicalrecordController.java
@@ -1,20 +1,25 @@
 package com.safetynet.safetynetalerts.controller;
 
-
 import com.safetynet.safetynetalerts.model.Medicalrecord;
 import com.safetynet.safetynetalerts.service.MedicalrecordService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MedicalrecordController {
 
     @Autowired
     private MedicalrecordService medicalrecordService;
 
     @GetMapping("/medicalrecords")
-    public Iterable<Medicalrecord> getMedicalrecords() {
-        return medicalrecordService.getMedicalrecords();
+    public Iterable<Medicalrecord> getMedicalrecords(Model model) {
+
+        Iterable<Medicalrecord> ListMedicalrecords = medicalrecordService.getMedicalrecords();
+
+        model.addAttribute("medicalrecords",ListMedicalrecords);
+
+        return ListMedicalrecords;
     }
 }

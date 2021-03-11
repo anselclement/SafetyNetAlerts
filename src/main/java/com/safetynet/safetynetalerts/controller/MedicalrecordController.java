bbd0fe76a9686c2a26b836d5b51e65bce6fc74bb
Clement Ansel <clem-nogare@hotmail.fr>
2021-03-11 10:54:47 +0100

    mise en place du delete, create, save du MedicalrecordController et de la vue

--- src/main/java/com/safetynet/safetynetalerts/controller/MedicalrecordController.java
+++ src/main/java/com/safetynet/safetynetalerts/controller/MedicalrecordController.java
@@ -1,11 +1,18 @@
 package com.safetynet.safetynetalerts.controller;
 
 import com.safetynet.safetynetalerts.model.Medicalrecord;
+import com.safetynet.safetynetalerts.model.Person;
 import com.safetynet.safetynetalerts.service.MedicalrecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class MedicalrecordController {
@@ -21,5 +28,31 @@ public class MedicalrecordController {
         model.addAttribute("medicalrecords",ListMedicalrecords);
 
         return ListMedicalrecords;
+    }
+
+    @GetMapping("/createMedicalrecord")
+    public String createMedicalrecord(Model model){
+        Medicalrecord medicalrecord = new Medicalrecord();
+        model.addAttribute("medicalrecord", medicalrecord);
+        return "/form/formNewMedicalRecord";
+    }
+
+    @PostMapping("/saveMedicalrecord")
+    public ModelAndView saveMedicalrecord(@ModelAttribute Medicalrecord medicalrecord){
+        medicalrecordService.saveMedicalrecord(medicalrecord);
+        return new ModelAndView("redirect:/medicalrecord");
+    }
+
+    @GetMapping("/updateMedicalrecord/{id}")
+    public String updateMedicalrecord(@PathVariable("id") final Long id, Model model){
+        Optional<Medicalrecord> medicalrecord = medicalrecordService.getMedicalrecord(id);
+        model.addAttribute("medicalrecord", medicalrecord);
+        return "/form/formUpdateMedicalRecord";
+    }
+
+    @GetMapping("/deleteMedicalrecord/{id}")
+    public ModelAndView deleteMedicalrecord(@PathVariable("id") final Long id){
+        medicalrecordService.deleteMedicalrecord(id);
+        return new ModelAndView("redirect:/medicalrecord");
     }
 }
